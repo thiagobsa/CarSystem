c0118d3ee5a5e0e9bfdabdd2d7990d9d51119ad2
thiagobsa <thiagobsa@gmail.com>
2024-05-23 09:33:35 -0300

    CarSystem-011: Inclusao do front-end
    Inclusao do front-end não concluido

---
+++ script.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user (first_name, last_name, birthday, login, email, password, phone, created_at) VALUES ('Claudia', 'Silva', '1986-05-03', 'claudia.silva', 'claudia.silva@hotmail.com', 'claudia123', '4533678894', NOW());
+
+INSERT INTO tb_car (year, license_plate, model, color, user_id) VALUES (2006, 'WKD009', 'Uno Mile', 'Gray', 1);
